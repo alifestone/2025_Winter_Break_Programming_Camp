e59264bbfa34f423b816f42225790283b6a68fdc
Mahiro <95558265+mahiro96@users.noreply.github.com>
2025-01-20 17:35:19 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,42 @@
-basic.showNumber(3)
-basic.pause(1000)
-basic.showNumber(2)
-basic.pause(1000)
-basic.showNumber(1)
-basic.pause(1000)
-if (true) {
-	
-} else {
-	
-}
+input.onButtonPressed(Button.A, function () {
+    airplane.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    airplane.change(LedSpriteProperty.X, 1)
+})
+let bullet: game.LedSprite = null
+let enemy: game.LedSprite = null
+let airplane: game.LedSprite = null
+airplane = game.createSprite(2, 4)
+game.setLife(3)
 basic.forever(function () {
-	
+    enemy = game.createSprite(randint(0, 4), 0)
+    while (true) {
+        basic.pause(1000)
+        enemy.change(LedSpriteProperty.Y, 1)
+        if (bullet.isTouching(enemy)) {
+            enemy.delete()
+            break;
+        }
+        if (enemy.isTouchingEdge() || airplane.isTouching(enemy)) {
+            enemy.delete()
+            game.removeLife(1)
+            break;
+        }
+    }
+})
+basic.forever(function () {
+    bullet = game.createSprite(airplane.get(LedSpriteProperty.X), airplane.get(LedSpriteProperty.Y))
+    while (true) {
+        basic.pause(200)
+        bullet.change(LedSpriteProperty.Y, -1)
+        if (bullet.isTouchingEdge()) {
+            bullet.delete()
+            break;
+        }
+        if (bullet.isTouching(enemy)) {
+            bullet.delete()
+            break;
+        }
+    }
 })
