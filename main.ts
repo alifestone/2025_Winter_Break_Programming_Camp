ee07937f08819ec7a9e441560b669546bec8ec99
alifestone <95862568+alifestone@users.noreply.github.com>
2025-01-20 16:14:32 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+if (true) {
+	
+} else {
+	
+}
+basic.forever(function () {
+	
+})
